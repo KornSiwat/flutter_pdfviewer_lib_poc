e87d6e1e480dd5683d430974aaf8b6e49a29042b
Siwat Ponpued <siwatponpued@gmail.com>
2023-02-16 17:47:52 +0700

    use flutter_pdfview with path_provider lib

---
+++ android/app/src/main/kotlin/com/example/pdf_viewer_poc_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pdf_viewer_poc_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,196 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> urls = [
+    "https://storage.googleapis.com/sca-public/gn-app/28CL%20Pricelist%20GY_110123.pdf",
+    "https://storage.googleapis.com/sca-public/gn-app/28CL%20Pricelist%20Std%20unit_110123.pdf",
+    "https://international.scasset.com/wp/wp-content/uploads/2020/03/28-Chidlom_TheTower_E-Brochure-1.pdf",
+    "https://international.scasset.com/wp/wp-content/uploads/2020/03/28-Chidlom_TheVilla_E-Brochure.pdf",
+  ];
+  final paths = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    urls.forEach(
+      (url) => createFileOfPdfUrl(url: url).then((f) {
+        setState(() {
+          paths.add(f.path);
+        });
+      }),
+    );
+  }
+
+  Future<File> createFileOfPdfUrl({required String url}) async {
+    Completer<File> completer = Completer();
+    print("Start download file from internet!");
+    try {
+      final filename = url.substring(url.lastIndexOf("/") + 1);
+      var request = await HttpClient().getUrl(Uri.parse(url));
+      var response = await request.close();
+      var bytes = await consolidateHttpClientResponseBytes(response);
+      var dir = await getApplicationDocumentsDirectory();
+      print("Download files");
+      print("${dir.path}/$filename");
+      File file = File("${dir.path}/$filename");
+
+      await file.writeAsBytes(bytes, flush: true);
+      completer.complete(file);
+    } catch (e) {
+      throw Exception('Error parsing asset file!');
+    }
+
+    return completer.future;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter PDF View',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Plugin example app')),
+        body: Center(child: Builder(
+          builder: (BuildContext context) {
+            return Column(
+              children: <Widget>[
+                ...paths.map(
+                  (path) => TextButton(
+                    child: const Text("Remote PDF"),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PDFScreen(path: path),
+                        ),
+                      );
+                    },
+                  ),
+                )
+              ],
+            );
+          },
+        )),
+      ),
+    );
+  }
+}
+
+class PDFScreen extends StatefulWidget {
+  final String? path;
+
+  const PDFScreen({Key? key, this.path}) : super(key: key);
+
+  @override
+  createState() => _PDFScreenState();
+}
+
+class _PDFScreenState extends State<PDFScreen> with WidgetsBindingObserver {
+  final Completer<PDFViewController> _controller =
+      Completer<PDFViewController>();
+  int? pages = 0;
+  int? currentPage = 0;
+  bool isReady = false;
+  String errorMessage = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Document"),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.share),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Stack(
+        children: <Widget>[
+          PDFView(
+            filePath: widget.path,
+            enableSwipe: true,
+            swipeHorizontal: true,
+            autoSpacing: false,
+            pageFling: true,
+            pageSnap: true,
+            defaultPage: currentPage!,
+            fitPolicy: FitPolicy.BOTH,
+            preventLinkNavigation:
+                false, // if set to true the link is handled in flutter
+            onRender: (_pages) {
+              setState(() {
+                pages = _pages;
+                isReady = true;
+              });
+            },
+            onError: (error) {
+              setState(() {
+                errorMessage = error.toString();
+              });
+              print(error.toString());
+            },
+            onPageError: (page, error) {
+              setState(() {
+                errorMessage = '$page: ${error.toString()}';
+              });
+              print('$page: ${error.toString()}');
+            },
+            onViewCreated: (PDFViewController pdfViewController) {
+              _controller.complete(pdfViewController);
+            },
+            onLinkHandler: (String? uri) {
+              print('goto uri: $uri');
+            },
+            onPageChanged: (int? page, int? total) {
+              print('page change: $page/$total');
+              setState(() {
+                currentPage = page;
+              });
+            },
+          ),
+          errorMessage.isEmpty
+              ? !isReady
+                  ? const Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : Container()
+              : Center(
+                  child: Text(errorMessage),
+                )
+        ],
+      ),
+      floatingActionButton: FutureBuilder<PDFViewController>(
+        future: _controller.future,
+        builder: (context, AsyncSnapshot<PDFViewController> snapshot) {
+          if (snapshot.hasData) {
+            return FloatingActionButton.extended(
+              label: Text("Go to ${pages! ~/ 2}"),
+              onPressed: () async {
+                await snapshot.data!.setPage(pages! ~/ 2);
+              },
+            );
+          }
+
+          return Container();
+        },
+      ),
+    );
+  }
+}
